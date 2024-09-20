d820403545f8afc2d41b0ac08f7c310b02ed4c85
Stiven <stivenhr21@gmail.com>
2024-09-20 09:13:33 -0500

    Se sube cambio en el elemento BTN_INGRESAR de la clase InicioSesionUserinterface

--- src/main/java/co/com/choucair/userinterface/iniciosesion/InicioSesionUserinterface.java
+++ src/main/java/co/com/choucair/userinterface/iniciosesion/InicioSesionUserinterface.java
@@ -4,8 +4,8 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class InicioSesionUserinterface {
-    public static final Target NOMBRE_USUARIO_LOGUEADO = Target.the("Nombre de usuario logueado").located(By.xpath("//p[text() = 'manda user']"));
     public static final Target INPUT_USUARIO = Target.the("Input para diligenciar usuario a loguearse").located(By.name("username"));
     public static final Target INPUT_CLAVE = Target.the("Input para diligenciar clave de usuario a loguearse").located(By.name("password"));
-    public static final Target BTN_INGRESAR = Target.the("Boton ingresar al sistema").located(By.xpath("//button[@class = 'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']"));
+    public static final Target BTN_INGRESAR = Target.the("Boton ingresar al sistema").located(By.xpath("//button[text()= ' Login ']"));
+    public static final Target NOMBRE_USUARIO_LOGUEADO = Target.the("Nombre de usuario logueado").located(By.xpath("//p[text() = 'manda user']"));
 }
